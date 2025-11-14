a3105febb116c8198adbbe3317a7688b3b2af73c
BrunoH <riquebruno@yahoo.com.br>
2025-11-14 20:38:27 -0300

    feat: add route index file

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import mainRoute from './routes/index.js';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(mainRoute);
 
 export default app;
