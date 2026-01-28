52f0d7e15ba8702863ec821f8f752842399274bf
Bruno Henrique <riquebruno@yahoo.com.br>
2026-01-27 22:10:01 -0300

    feat: add LinkController with createLink method for link management

---
+++ src/controller/LinkController.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from "express";
+import LinkService from "../services/LinkService.js";
+
+export default class LinkController {
+    private linkService: LinkService;
+
+    constructor() {
+        this.linkService = new LinkService();
+    }
+
+    // POST /links
+    async createLink(req: Request, res: Response) {
+        const {title, url} = req.body;
+        const result = await this.linkService.create(title, url);
+        return res.status(result.status).json(result.data);
+    }
+
+}
