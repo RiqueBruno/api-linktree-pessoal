f54a4cd3bdc0fe35f661d9f474921dcaf3db1afa
Bruno Henrique <riquebruno@yahoo.com.br>
2026-01-28 19:14:47 -0300

    feat: add getLinkById method to LinkController for retrieving a link by ID

--- src/controller/LinkController.ts
+++ src/controller/LinkController.ts
@@ -6,19 +6,25 @@ export default class LinkController {
 
     constructor() {
         this.linkService = new LinkService();
-    }
+    };
 
     // POST /links
     async createLink(req: Request, res: Response) {
         const {title, url} = req.body;
         const result = await this.linkService.create(title, url);
         return res.status(result.status).json(result.data);
-    }
+    };
 
     // GET /links
     async getAllLinks(req: Request, res: Response) {
         const result = await this.linkService.getAll();
         return res.status(result.status).json(result.data);
-    }
+    };
 
-}
+    // GET /links/:id
+    async getLinkById(req: Request, res: Response) {
+        const { id } = req.params;
+        const result = await this.linkService.getById(Number(id));
+        return res.status(result.status).json(result.data);
+    };
+};
