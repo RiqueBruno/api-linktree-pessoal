fba6567506000bec4dcb7716ccb057d45a750156
Bruno Henrique <riquebruno@yahoo.com.br>
2026-01-29 18:57:10 -0300

    feat: add PUT route for updating a link by ID in linkRoutes

--- src/routes/link.routes.ts
+++ src/routes/link.routes.ts
@@ -13,6 +13,9 @@ linkRoutes.get("/", (req, res) => linkController.getAllLinks(req, res));
 // GET /links/:id
 linkRoutes.get("/:id", (req, res) => linkController.getLinkById(req, res));
 
+// PUT /links/:id
+linkRoutes.put("/:id", (req, res) => linkController.updateLinkById(req, res));
+
 // DELETE /links/:id
 linkRoutes.delete("/:id", (req, res) => linkController.deleteLinkById(req, res));
 
